ad860e87495732d1d566dbd66af6e76b1b46a691
SNCSamitek <123840779+SNCSamitek@users.noreply.github.com>
2026-01-21 18:02:20 -0500

    Add files via upload

---
+++ Assignemnt1/Assignemnt1/guests.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "date.h"
+#include "information.h"
+#include <string>
+
+class Guests {
+
+private:
+	Date checkIn;
+	Date checkOut;
+
+	Information ;
+public: 
+
+};
---
+++ Assignemnt1/Assignemnt1/information.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include "information.h"
+#include "date.h"
+
+class Information {
+
+private:
+	char firstName[2] = {};
+	char lastName[2] = {};
+	Date dateOfBirth;
+
+public:
+
+};

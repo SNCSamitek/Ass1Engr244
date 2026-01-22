bea6a937dfde509e35de02997f6d5cff8f51cf32
SNCSamitek <samorozcoestrada@gmail.com>
2026-01-22 13:01:24 -0500

    started reseveation table search for display details

--- reservation_manager.cpp
+++ reservation_manager.cpp
@@ -1,8 +1,25 @@
 #include "reservation_manager.h"
 
 Reservation_Manager::Reservation_Manager(){
-    const static int max_no_of_nights = 7;
-	const static int no_of_rooms = 20;
-	Guests_Res_Request *arr;
 	int reservation_table [max_no_of_nights][no_of_rooms] = {};
+}
+
+Reservation_Manager::~Reservation_Manager(){
+    delete arr;
+}
+
+void Reservation_Manager::displayDetails(int id){
+    for(int i = 0; i < no_of_rooms; i++){
+        if(id == reservation_table[j][i]){
+            
+        }
+    }
+}
+
+int Reservation_Manager::processReservation(const Guests_Res_Request){
+
+}
+
+void Reservation_Manager::cancelReservation(int id){
+
 }
